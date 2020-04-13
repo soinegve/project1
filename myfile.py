1c1a10870658d72842a266520305bbe059279148
karolos-evgenios <7596587+karolos-evgenios@users.noreply.github.com>
2020-04-12 20:11:59 -0400

    sorry for saying array . I meant list

--- myfile.py
+++ myfile.py
@@ -1,1 +1,19 @@
-test
+from array import array
+
+a = list([1,2,3])
+print(a)
+print(type(a))
+
+b = [1,2,3]
+print(b)
+print(type(b))
+
+
+c = array('d', [1, 2, 3, 4, 5])
+print(c)
+print(type(c))
+
+
+d = array('l', [1, 2, 3, 4, 5])
+print(d)
+print(type(d))
